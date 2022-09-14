d4a7faf240a5f35ad29be6cb3547a665b1d5924e
VishalVenkat28 <67194989+VishalVenkat28@users.noreply.github.com>
2022-09-14 12:16:00 +0530

    Ok

---
+++ build/precache-manifest.72a4e88ebf652da50aaf107edf53e4db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "041143618af47f1831b0",
+    "url": "/Estore-react-reduxtoolkit/static/css/main.f28f9cd6.chunk.css"
+  },
+  {
+    "revision": "041143618af47f1831b0",
+    "url": "/Estore-react-reduxtoolkit/static/js/main.51850847.chunk.js"
+  },
+  {
+    "revision": "17c2e32d19eaf32247e9",
+    "url": "/Estore-react-reduxtoolkit/static/js/runtime~main.cc1c1036.js"
+  },
+  {
+    "revision": "df5626325147f2865272",
+    "url": "/Estore-react-reduxtoolkit/static/css/2.d2ae4421.chunk.css"
+  },
+  {
+    "revision": "df5626325147f2865272",
+    "url": "/Estore-react-reduxtoolkit/static/js/2.06eb6270.chunk.js"
+  },
+  {
+    "revision": "0d084339b676cfedeccc4fdbbb468303",
+    "url": "/Estore-react-reduxtoolkit/static/media/pexels-fwstudio-129731.0d084339.jpg"
+  },
+  {
+    "revision": "231aaefe8eab6979fe6df020b7007cbb",
+    "url": "/Estore-react-reduxtoolkit/index.html"
+  }
+];
